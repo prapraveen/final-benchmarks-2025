f84a93f14566431298f2a465f3217c5d477c95ad
Kevin Pan <kevin.yonghao.pan@gmail.com>
2025-12-03 14:32:52 -0500

    update tests

--- benchmarks/kevin-pan-221-test1.lisp
+++ benchmarks/kevin-pan-221-test1.lisp
@@ -2,7 +2,7 @@
     (if (= x 0) 
         0 
         (+ x (sum (sub1 x)))))
-(let ((a (sum 100)))
-    (let ((b (+ (sum 100) (sum 10))))
-        (let ((c (+ (+ (sum 100) (sum 10)) (sum 20))))
+(let ((a (sum 15)))
+    (let ((b (+ (sum 15) (sum 10))))
+        (let ((c (+ (+ (sum 15) (sum 10)) (sum 20))))
             (print (+ (+ a b)) c))))
